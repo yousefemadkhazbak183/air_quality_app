a71c46dd5a5ac86cd42f56295a83c5020ee666f0
Yousef Emad <yousefemadkhazbak@gmail.com>
2026-04-10 11:40:04 +0200

    feat(domain): add domain layer - entities, failures, repository interface and usecases
    - Add Failure sealed class with required message parameter
    - Add SensorReading entity with all ESP8266 sensor fields
    - Add dominantGasLevel computed getter for charts
    - Add SensorRepository abstract interface
    - Add FetchSensorReadingUseCase and GetHistoricalReadingsUseCase

---
+++ lib/domain/entities/sensor_reading.dart
@@ -0,0 +1,48 @@
+import 'package:equatable/equatable.dart';
+
+import '../../core/enums/air_quality_status.dart';
+
+final class SensorReading extends Equatable {
+  const SensorReading({
+    required this.temperature,
+    required this.humidity,
+    required this.methane,
+    required this.propane,
+    required this.butane,
+    required this.smoke,
+    required this.alcohol,
+    required this.airQualityStatus,
+    required this.timestamp,
+  });
+
+  final double temperature;
+  final double humidity;
+  final double methane;
+  final double propane;
+  final double butane;
+  final double smoke;
+  final double alcohol;
+  final AirQualityStatus airQualityStatus;
+  final DateTime timestamp;
+
+  double get dominantGasLevel => [
+    methane,
+    propane,
+    butane,
+    smoke,
+    alcohol,
+  ].reduce((a, b) => a > b ? a : b);
+
+  @override
+  List<Object?> get props => [
+    temperature,
+    humidity,
+    methane,
+    propane,
+    butane,
+    smoke,
+    alcohol,
+    airQualityStatus,
+    timestamp,
+  ];
+}
---
+++ lib/domain/usecases/fetch_sensor_reading_usecase.dart
@@ -0,0 +1,13 @@
+import 'package:air_high_quality_app/domain/entities/sensor_reading.dart';
+import 'package:air_high_quality_app/domain/failures/failure.dart';
+import 'package:air_high_quality_app/domain/repositories/sensor_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+final class FetchSensorReadingUsecase {
+  FetchSensorReadingUsecase(this._sensorRepository);
+
+  final SensorRepository _sensorRepository;
+
+  Future<Either<Failure, SensorReading>> call() =>
+      _sensorRepository.fetchLatestReading();
+}
